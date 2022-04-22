bb788eef25e454b1086fc1f919c1a002f79d521b
Ajay Patel <ajaypatelkota@gmail.com>
2022-04-23 00:59:52 +0530

    Random

--- techAgainstCrime/settings.py
+++ techAgainstCrime/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.AppConfig'
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
