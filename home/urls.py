2769c41226666246c60563497c5c59f4ce0fd0a7
utkrisht5 <utkrishtsingh5@gmail.com>
2022-04-23 00:39:05 +0530

    Adding authentication files

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('victimSignup', views.victimSignup, name="victimSignup"),
+    path('victimLogin', views.victimLogin, name='victimLogin'),
+    path('authoritySignup', views.authoritySignup, name="authoritySignup"),
+    path('authorityLogin', views.authorityLogin, name="authorityLogin"),
+    path('authorityHome', views.authorityHome, name="authorityHome"),
+    path('victimHome', views.victimHome, name="victimHome"),
+    path('victimLogout', views.victimLogout, name="victimLogout"),
+    path('authorityLogout', views.authorityLogout, name="authorityLogout"),
 ]
