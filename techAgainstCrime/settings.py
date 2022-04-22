26beee84946eb3e43bbf4263a2e88e9d66322bb2
utkrisht5 <utkrishtsingh5@gmail.com>
2022-04-22 22:34:17 +0530

    Starting

--- techAgainstCrime/settings.py
+++ techAgainstCrime/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.AppConfig'
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
